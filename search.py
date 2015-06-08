ee813d021521976823f9898403af80ffbad73410
elmsly <almelid@pvv.ntnu.no>
2015-06-08 22:19:31 +0100

    I need to read the python style guide slightly more frequently than I do

--- search.py
+++ search.py
@@ -4,124 +4,118 @@ from HTMLParser import HTMLParser
 
 #testRequest = requests.get("http://inspirehep.net/search?ln=en&ln=en&p=find+eprint+1506.01386&of=hx&action_search=Search&sf=earliestdate&so=d&rm=&rg=25&sc=0")
 
-class spiresFormat():
+class SpiresFormat():
 #	formatRequestPrefix="&of="
-	def __init__(self,name,formatString,recordTag):
+	def __init__(self,name,format_string,record_tag):
 		self.name=name
-		self.formatString=formatString
-		self.recordTag=recordTag
+		self.format_string=format_string
+		self.record_tag=record_tag
 
 #	def formatRequest(self):
 #		return self.formatRequestPrefix+self.formatString
 
-spiresBibtex=spiresFormat("bibtex","hx","pre")
+spiresBibtex=SpiresFormat("bibtex","hx","pre")
+spiresJson=SpiresFormat("json","recjson","")
 
-class spiresSearch():
-	requestStringPre="http://inspirehep.net/search?"
-	requestStringPost=""
+class SpiresSearch():
+	inspire_url="http://inspirehep.net/search?"
 
-	def __init__(self,searchString,dataFormat=spiresBibtex,chunkSize=25,jump=1,
-			staticOptions={"sf" : "earliestdate"}):
+	def __init__(self,search_string,data_format=spiresBibtex,chunk_size=25,jump=1,
+			static_options={"sf" : "earliestdate"}):
 
-		self.chunkSize=chunkSize
+		self.chunk_size=chunk_size
 		self.jump=jump
-		self.dataFormat=dataFormat
-		self.staticOptions=staticOptions
-#		self.updateOptions()
-		self.searchString=searchString
-		self.generateRequestString()
+		self.data_format=data_format
+		self.static_options=static_options
+		self.search_string=search_string
+		self.generate_request_string()
 
-		self.parser=spiresHTMLParser(dataFormat)
-		self.requestedFlag=False
+		self.parser=SpiresHTMLParser(data_format)
+		self.requested_flag=False
 
-	def updateOptions(self):
-		self.searchOptions=self.staticOptions
-		self.searchOptions["of"]=self.dataFormat.formatString
-		self.searchOptions["rg"]=str(self.chunkSize)
-		self.searchOptions["jrec"]=str(self.jump)
+	def update_options(self):
+		self.search_options=self.static_options
+		self.search_options["of"]=self.data_format.format_string
+		self.search_options["rg"]=str(self.chunk_size)
+		self.search_options["jrec"]=str(self.jump)
 
-	def generateRequestString(self):
+	def generate_request_string(self):
 		# generates the actual spires search string 
-		self.requestString=self.requestStringPre+"&p="+self.searchString
-#		self.requestString+=dataFormat.formatRequest()
+		self.request_string=self.inspire_url+"&p="+self.search_string
 		# adds the options, including the data format
-		self.updateOptions()
-		for key, value in self.searchOptions.items():
-			self.requestString+="&"+key+"="+value
+		self.update_options()
+		for key, value in self.search_options.items():
+			self.request_string+="&"+key+"="+value
 
-		self.requestString+=self.requestStringPost
 
-	def getPreviousChunk(self):
-		if self.jump>self.chunkSize:
-			self.jump-=self.chunkSize
+
+	def get_previous_chunk(self):
+		if self.jump>self.chunk_size:
+			self.jump-=self.chunk_size
 		else:
 			print "chunk size larger than offset. Resetting search to first chunk"
 			self.jump=1
-#		self.updateOptions()
-#		self.generateRequestString()
-		self.performRequest()
+		self.perform_request()
 
-	def getNextChunk(self):
-		self.jump+=self.chunkSize
-#		self.updateOptions()
-#		self.generateRequestString()
-		self.performRequest()
+	def get_next_chunk(self):
+		self.jump+=self.chunk_size
+		self.perform_request()
 		
-	def performRequest(self):
-		self.generateRequestString()
-		self.requestedFlag=True
-		self.request=requests.get(self.requestString)
+	def perform_request(self):
+		self.generate_request_string()
+		self.requested_flag=True
+		self.request=requests.get(self.request_string)
 
-	def parseRequest(self):
-		if not self.requestedFlag:
-			self.performRequest()
-		return self.parser.getTagData(self.request.text)
+	def parse_request(self):
+		if not self.requested_flag:
+			self.perform_request()
+		return self.parser.get_tag_data(self.request.text)
 
-class spiresBibtexSearch(spiresSearch):
+class SpiresBibtexSearch(SpiresSearch):
 
-	def __init__(self,searchString,chunkSize=25,jump=1,
-			staticOptions={"sf" : "earliestdate"}):
-		spiresSearch.__init__(self,searchString,dataFormat=spiresBibtex,
-					chunkSize=chunkSize,jump=jump,
-					staticOptions=staticOptions)
+	def __init__(self,search_string,chunk_size=25,jump=1,
+			static_options={"sf" : "earliestdate"}):
+		SpiresSearch.__init__(self,search_string,data_format=spiresBibtex,
+					chunk_size=chunk_size,jump=jump,
+					static_options=static_options)
 
-class spiresBibtexEprintSearch(spiresBibtexSearch):
-	def __init__(self,eprintNum):
-		spiresBibtexSearch.__init__(self,"find eprint+"+eprintNum)
+class SpiresBibtexEprintSearch(SpiresBibtexSearch):
+	def __init__(self,eprint_num):
+		SpiresBibtexSearch.__init__(self,"find eprint+"+eprint_num)
 
 
-class spiresBibtexDoiSearch(spiresBibtexSearch):
+class SpiresBibtexDoiSearch(SpiresBibtexSearch):
 	def __init__(self,doi):
-		spiresBibtexSearch.__init__(self,"find doi+"+doi)
+		SpiresBibtexSearch.__init__(self,"find doi+"+doi)
 		
 
-class spiresHTMLParser(HTMLParser):
-	def __init__(self,dataFormat):
+class SpiresHTMLParser(HTMLParser):
+	def __init__(self,data_format):
 		HTMLParser.__init__(self)
-		self.preFlag=0
-		self.dataFormat=dataFormat
-		self.recordTag=dataFormat.recordTag
+		self.pre_flag=False
+		self.data_format=data_format
+		self.record_tag=data_format.record_tag
 
-	def getTagData(self,text):
+	def get_tag_data(self,text):
 		self.results=[]
 		self.feed(text)
 		return self.results
 
 	def handle_starttag(self,tag,attrs):
-		if tag==self.recordTag:
+		if tag==self.record_tag:
 #			print "found start-tag "+tag
-			self.preFlag=True
+			self.pre_flag=True
 	
 	def handle_endtag(self,tag):
-		if tag==self.recordTag:
+		if tag==self.record_tag:
 #			print "found end-tag "+tag
-			self.preFlag=False
+			self.pre_flag=False
 
 	def handle_data(self,data):
-		if self.preFlag:
-#			print "Found bibtex: "+data
+		if self.pre_flag:
+			print "Found bibtex: "+data
 			self.results.append(data)
 
 
-#testSearch=spiresBibtexSearch("find+a+gardi")
-testSearch=spiresBibtexEprintSearch("0709.2877")
+#test_search=SpiresBibtexSearch("find+a+gardi")
+test_search=SpiresBibtexEprintSearch("0709.2877")
